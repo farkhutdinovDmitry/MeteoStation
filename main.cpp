f0db778156a2a02202ab4037977cb098c554c0f4
farkhutdinovq <d.farkhutdinov@g.nsu.ru>
2021-05-24 20:53:51 +0700

    лямбда внутри таймера

--- main.cpp
+++ main.cpp
@@ -6,19 +6,17 @@ Ticker loopTicker;
 BmeWrapper bme;
 State *state;
 
-void loopTickerFunction() {
-  state->setTemp(bme.readTemperature());
-  state->setHumidity(bme.readHumidity());
-  state->setPressure(bme.readPressure());
-}
-
 void setup() {
   BrokerCredentials brokerCredentials("test.mosquitto.org", 1883);
   WiFiCredentials wiFiCredentias("TP-Link_513C", "55955430");
   EspNetwork network(brokerCredentials, wiFiCredentias);
   network.connect();
   state = new State(network);
-  loopTicker.attach(10, loopTickerFunction);
+  loopTicker.attach(10, []() {
+    state->setTemp(bme.readTemperature());
+    state->setHumidity(bme.readHumidity());
+    state->setPressure(bme.readPressure());
+  });
 }
 
 void loop() {}
